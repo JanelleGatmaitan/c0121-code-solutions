38cc42bf69408d8658b9aadd8bc472fa763de287
Janelle Gatmaitan <janellegatmaitan@gmail.com>
2021-02-04 15:29:58 -0800

    completed dom-event-delegation

---
+++ dom-event-delegation/main.js
@@ -0,0 +1,11 @@
+var $ul = document.querySelector('.task-list');
+$ul.addEventListener('click', function (event) {
+  console.log('event.target: ', event.target);
+  console.log('event.target.tagName: ', event.target.tagName);
+  if (event.target.tagName === 'BUTTON') {
+    var closestElement = event.target.closest('.task-list-item');
+    console.log('closest .task-list-item: ', closestElement);
+    var $li = document.querySelector('.task-list-item');
+    $li.remove(closestElement);
+  }
+});
