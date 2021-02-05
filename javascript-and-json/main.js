9f0905f043e8d7df9844c3c435e84dc89e5861d9
Janelle Gatmaitan <janellegatmaitan@gmail.com>
2021-02-05 13:23:29 -0800

    completed javascript-and-json

---
+++ javascript-and-json/main.js
@@ -0,0 +1,37 @@
+var library = [
+  {
+    isbn: '123',
+    title: 'To Kill a Mockingbird',
+    author: 'Harper Lee'
+  },
+
+  {
+    isbn: '456',
+    title: 'The Scarlet Letter',
+    author: 'Nathaniel Hawthorne'
+  },
+
+  {
+    isbn: '789',
+    title: 'The Outsiders',
+    author: 'S.E. Hinton'
+  }
+];
+console.log('library: ', library);
+console.log('typeof library: ', typeof library);
+
+var libraryString = JSON.stringify(library);
+console.log('JSON.stringify: ', libraryString);
+console.log('typeof libraryString: ', typeof libraryString);
+
+var student = JSON.stringify(
+    name: 'Janelle',
+    id: 1550
+);
+
+console.log('student: ', student);
+console.log('typeof student: ', typeof student);
+
+var parsed = JSON.parse(student);
+console.log('parsed student: ', parsed);
+console.log('typeof parsed: ', typeof parsed);
