54d4b03e682aeb5daca8215553060fd63d506e10
Janelle Gatmaitan <janellegatmaitan@gmail.com>
2021-02-06 14:08:24 -0800

    removed closestELement

--- dom-event-delegation/main.js
+++ dom-event-delegation/main.js
@@ -1,12 +1,13 @@
 var $ul = document.querySelector('.task-list');
-var $li = document.querySelector('.task-list-item');
 
 $ul.addEventListener('click', function (event) {
+
   console.log('event.target: ', event.target);
   console.log('event.target.tagName: ', event.target.tagName);
+
   if (event.target.tagName === 'BUTTON') {
     var closestElement = event.target.closest('.task-list-item');
     console.log('closest .task-list-item: ', closestElement);
-    $li.remove(closestElement);
+    closestElement.remove();
   }
 });
