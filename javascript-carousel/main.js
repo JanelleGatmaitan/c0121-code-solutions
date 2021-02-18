a86038cfbdf440df7ec3fe10d097d655f4d27013
Janelle Gatmaitan <janellegatmaitan@gmail.com>
2021-02-17 20:21:20 -0800

    working leftslide function

--- javascript-carousel/main.js
+++ javascript-carousel/main.js
@@ -2,22 +2,49 @@ var $chevronRight = document.querySelector('.fa-chevron-right');
 var $chevronLeft = document.querySelector('.fa-chevron-left');
 var $images = document.querySelectorAll('img');
 var $circles = document.querySelectorAll('i.fa-circle');
-var currentIndex = 1;
+var index = 0;
+var lastIndex = $images.length - 1;
 
 function slideRight () {
   for (var i = 0; i < $images.length; i++) {
     $images[i].className = 'slide-hidden';
     $circles[i].className = 'far fa-circle'
   }
-  if (currentIndex < $images.length) {
-    $images[currentIndex].className = 'current-slide';
-    $circles[currentIndex].className = 'fas fa-circle'
-    currentIndex++;
+  if (index < lastIndex) {
+    $images[index + 1].className = 'current-slide';
+    $circles[index + 1].className = 'fas fa-circle'
+    index++;
   } else {
     $images[0].className = 'current-slide';
     $circles[0].className = 'fas fa-circle'
-    currentIndex = 1;
+    index = 0;
   }
 }
 
+function slideLeft() {
+  for (var i = 0; i < $images.length; i++) {
+    $images[i].className = 'slide-hidden';
+    $circles[i].className = 'far fa-circle'
+    console.log('loop');
+  }
+  if (index > 0) {
+    $images[index - 1].className = 'current-slide';
+    $circles[index - 1].className = 'fas fa-circle'
+    index--;
+  } else {
+    $images[lastIndex].className = 'current-slide';
+    $circles[lastIndex].className = 'fas fa-circle'
+    index = lastIndex;
+  }
+}
+
+
 $chevronRight.addEventListener('click', slideRight);
+$chevronLeft.addEventListener('click', slideLeft);
+  // } else if (currentIndex = 0) {
+  // console.log('yeet');
+  // currentIndex = $images.length - 1;
+  // $images[currentIndex].className = 'current-slide';
+  // $circles[currentIndex].className = 'fas fa-circle'
+  // currentIndex--;
+// }
