16c261f46f9dd84094be13cb789a6bfd10def5b7
Janelle Gatmaitan <janellegatmaitan@gmail.com>
2021-02-17 16:42:14 -0800

    circles match current slide

--- javascript-carousel/main.js
+++ javascript-carousel/main.js
@@ -1,18 +1,21 @@
 var $chevronRight = document.querySelector('.fa-chevron-right');
 var $chevronLeft = document.querySelector('.fa-chevron-left');
 var $images = document.querySelectorAll('img');
+var $circles = document.querySelectorAll('i.fa-circle');
 var currentIndex = 1;
 
 function slideRight () {
-  console.log('yeet');
   for (var i = 0; i < $images.length; i++) {
     $images[i].className = 'slide-hidden';
+    $circles[i].className = 'far fa-circle'
   }
   if (currentIndex < $images.length) {
     $images[currentIndex].className = 'current-slide';
+    $circles[currentIndex].className = 'fas fa-circle'
     currentIndex++;
   } else {
     $images[0].className = 'current-slide';
+    $circles[0].className = 'fas fa-circle'
     currentIndex = 1;
   }
 }
