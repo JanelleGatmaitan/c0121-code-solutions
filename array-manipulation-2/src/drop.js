b088f2a71b0e801cc06df0b5370f2106ca544e5d
Janelle Gatmaitan <janellegatmaitan@gmail.com>
2021-03-15 19:51:30 -0700

    completed array-manipulation-2

--- array-manipulation-2/src/drop.js
+++ array-manipulation-2/src/drop.js
@@ -2,23 +2,9 @@
 function drop(array, count) {
   var newArray = [];
   for (var i = 0; i < array.length; i++) {
-    if (array.indexOf(array[i]) >= count) {
+    if (i >= count) {
     newArray.push(array[i]);
     }
   }
   return newArray;
 }
-
-// for (var i = 0; i < count; i++) {
-//   delete array[i];
-// }
-// return array;
-
-// for (var i = 0; i < array.lenth; i++) {
-//   if
-// }
-// array.forEach(function checkElement(element) {
-//   if (array.indexOf(element) >= count) {
-//     newArray.push(element);
-//   }
-// });
