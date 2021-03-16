e34a6ce6f9ec6bb0d77ba01309dc0a6257744d25
Janelle Gatmaitan <janellegatmaitan@gmail.com>
2021-03-15 17:22:16 -0700

    completed express-get-json

---
+++ express-get-json/index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express();
+let grades = {
+  12: {
+    id: 12,
+    name: 'Tim Berners-Lee',
+    course: 'HTML',
+    score: 95
+  },
+  47: {
+    id: 47,
+    name: 'Brendan Eich',
+    course: 'JavaScript',
+    score: 100
+  },
+  273: {
+    id: 273,
+    name: 'Forbes Lindsay',
+    course: 'JavaScript',
+    score: 92
+  }
+};
+
+app.use('/api/grades', function (req, res) {
+  let array = [];
+  for (const property in grades) {
+    let grade = (grades[property]);
+    array.push(grade);
+  }
+  res.json(array);
+});
+
+app.listen(3000, () => {
+  console.log('Express server listening on port 3000');
+});
