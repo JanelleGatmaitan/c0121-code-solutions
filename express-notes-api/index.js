d38ef3f13edc56ea329fa7466ae9840ac500ea13
Janelle Gatmaitan <janellegatmaitan@gmail.com>
2021-03-16 21:01:47 -0700

    user can get a single note by id

--- express-notes-api/index.js
+++ express-notes-api/index.js
@@ -2,20 +2,8 @@ const express = require('express');
 const parsedNotes = require('./data');
 const notes = parsedNotes.notes;
 const app = express();
-app.use(express.json());
 
-// let file = process.argv[2];
-// fs.readFile(`${file}`, 'utf8', (err, data) => {
-//   if (err) throw err;
-//   console.log(data);
-// // });
-// app.get('/api/notes', (req, res) => {
-//   const showNotes = fs.readFile('data.json', 'utf8', (err, data) => {
-//     if (err) throw err;
-//     for (const [key, value] of Object.entries(notes)) {
-//       console.log(`${key}: ${value}`);
-//     }
-// });
+app.use(express.json());
 
 app.get('/api/notes', function (req, res) {
   let array = [];
@@ -26,6 +14,18 @@ app.get('/api/notes', function (req, res) {
   res.json(array);
 });
 
+app.get('/api/notes/:id', function (req, res) {
+  const noteId = req.params.id;
+  res.json(notes[noteId]);
+});
+
 app.listen(3000, () => {
   console.log('Express server listening on port 3000');
 });
+// const noteId = req.params.id;
+// console.log('req.params: ', req.params);
+// if (noteId != undefined) {
+//   res.json(array.noteId);
+// } else {
+//   res.json(array);
+// }
