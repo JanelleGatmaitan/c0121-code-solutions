20e86a4226fee914e43465ffa7d7fe87c82e3056
Janelle Gatmaitan <janellegatmaitan@gmail.com>
2021-03-26 23:28:17 -0700

    completed react-stopwatch-component

--- react-stopwatch-component/src/stopwatch.jsx
+++ react-stopwatch-component/src/stopwatch.jsx
@@ -5,39 +5,61 @@ export default class Stopwatch extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      isPaused: true,
+      isPaused: false,
       seconds: 0
     };
     this.handleClick = this.handleClick.bind(this);
+    this.reset = this.reset.bind(this);
     this.buttonClass = this.buttonClass.bind(this);
   }
 
   buttonClass() {
     if (this.state.isPaused) {
-      return 'fas fa-play';
+      return 'fas fa-pause';
     }
-    return 'fas fa-pause';
+    return 'fas fa-play';
   }
 
-  secondsElapsed() {
-    return setInterval(() => {
-      this.state.seconds++}
-      , 1000);
+  reset() {
+    if (!this.state.isPaused) {
+      this.setState({
+        isPaused: this.state.isPaused,
+        seconds: 0
+      });
+    }
   }
 
   handleClick() {
+    if (!this.state.isPaused) {
+      return this.startTimer();
+    } else {
+      return this.stopTimer();
+    }
+  }
+
+  stopTimer() {
+    clearInterval(this.state.timerId);
     this.setState({
-      isPaused: !this.state.isPaused,
-      seconds: this.state.seconds + 1
+      isPaused: false,
+      seconds: this.state.seconds
     });
+  }
+
+  startTimer() {
+    let timerId = setInterval(() => {
+      this.setState({
+        isPaused: true,
+        seconds: this.state.seconds + 1,
+        timerId: timerId
+      });
+    }, 1000);
   }
 
   render() {
     const buttonClass = this.buttonClass();
-    const seconds = this.state.seconds;
     return (
       <>
-        <div className="watch">
+        <div className="watch" onClick={this.reset}>
           <div className="seconds">
             {this.state.seconds}
           </div>
