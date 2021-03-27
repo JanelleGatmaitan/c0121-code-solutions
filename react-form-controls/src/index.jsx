579085eeec1ebc8270a7597ccbc656aaf096deb0
Janelle Gatmaitan <janellegatmaitan@gmail.com>
2021-03-26 17:27:04 -0700

    value = this.state.email

--- react-form-controls/src/index.jsx
+++ react-form-controls/src/index.jsx
@@ -26,7 +26,7 @@ class NewsLetterForm extends React.Component {
     return (
       <form onSubmit={this.handleSubmit}>
         <label>
-          Email: <input type="text" onChange={this.handleChange} value={this.state.value}/>
+          Email: <input type="text" onChange={this.handleChange} value={this.state.email}/>
         </label>
         <input type="submit" value="Submit" />
       </form>
