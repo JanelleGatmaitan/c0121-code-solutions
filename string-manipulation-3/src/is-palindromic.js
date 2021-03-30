4851376738ecf091ba76f56b138aba45077f6228
Janelle Gatmaitan <janellegatmaitan@gmail.com>
2021-03-30 15:15:46 -0700

    reversed string inPalidromic

---
+++ string-manipulation-3/src/is-palindromic.js
@@ -0,0 +1,8 @@
+/* exported isPalindromic */
+function isPalindromic(string) {
+  const reversed;
+  for (var i = word.length - 1; i >= 0; i--) {
+    reversed += word[i];
+  }
+console.log('reversed input string', reversed);
+}
